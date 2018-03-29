5b3e0d510f77bee0123bb0997656ad352cc87381
rdlucia <luciaracareanu@yahoo.com>
2018-03-29 15:41:01 +0200

    ajax simple

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 "use strict";
-// just a coment to test git v1
+var respJson;
+function makeAjaxCall(url, methodType){
+   var xhr = new XMLHttpRequest();
+   xhr.open(methodType, url, true);
+   xhr.send();
+   xhr.onreadystatechange = function(){
+     if (xhr.readyState === 4){
+        if (xhr.status === 200){
+           console.log("xhr done successfully");
+           var resp = xhr.responseText;
+           respJson = JSON.parse(resp);
+        } else {
+          console.log("xhr failed");
+        }
+     } else {
+        console.log("xhr processing going on");
+     }
+  }
+  console.log("request sent succesfully");
+}
+// git hub url to get btford details
+var URL = "https://api.github.com/users/btford";
+makeAjaxCall(URL, "GET");
+console.log (respJson);
